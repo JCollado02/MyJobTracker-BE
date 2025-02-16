2288fe76ecb123e5153ca3fe002c852d4161e280
JCollado02 <ji.sito122@gmail.com>
2025-02-15 22:05:56 -0500

    -Changed requestmapping url -added ability to get apps by id, add new apps, and to either update an app completely or partially

--- src/main/java/com/example/myjobtracker/jobapplication/JobAppController.java
+++ src/main/java/com/example/myjobtracker/jobapplication/JobAppController.java
@@ -1,15 +1,16 @@
 package com.example.myjobtracker.jobapplication;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 // Contains all resources for API, this is our API layer.
 @RestController  // makes class to serve rest endpoints
-@RequestMapping(path = "api/v1/jobapp")  // adds to end of localhost link
+@RequestMapping(path = "api/v1/job-applications")  // adds to end of localhost link
 public class JobAppController {
 
     private final JobAppService jobAppService;  // references our service
@@ -20,6 +21,35 @@ public class JobAppController {
     }
     @GetMapping
     public List<JobApp> getJobApps() {  // Server returns JSON array
-        return jobAppService.getJobApps();
+        return jobAppService.getAllJobApps();
+    }
+
+    @GetMapping("{id}")
+    public JobApp getJobAppsById(@PathVariable Integer id) {  // Finds job by given id
+        return jobAppService.getJobAppById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<JobApp> addNewJobApp(@RequestBody JobApp jobApp) {  // inserts new job
+        jobAppService.insertJobApp(jobApp);
+        return ResponseEntity.ok(jobApp);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteJobApp(@PathVariable Integer id) {  // deletes a specified job
+        jobAppService.deleteJobApp(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<JobApp> updateJobApp(@PathVariable Integer id, @RequestBody JobApp jobAppDetails) {
+        JobApp updatedJobApp = jobAppService.updateJobApp(id, jobAppDetails);
+        return ResponseEntity.ok(updatedJobApp);
+    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<JobApp> updateJobAppPartial(@PathVariable Integer id, @RequestBody Map<String, Object> updates) {  // partially updates a job
+        JobApp updatedJobApp = jobAppService.updateJobAppPartial(id, updates);
+        return ResponseEntity.ok(updatedJobApp);
     }
 }
