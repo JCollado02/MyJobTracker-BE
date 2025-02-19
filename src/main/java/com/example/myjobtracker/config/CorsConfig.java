206843e88d1ad810bfd916dd167edc6908ee2e8b
JCollado02 <ji.sito122@gmail.com>
2025-02-19 00:27:47 -0500

    added CorsConfig.java to let FE talk to BE

---
+++ src/main/java/com/example/myjobtracker/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.example.myjobtracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+// this class lets our FE talk to our BE
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("https://my-job-tracker-fe.vercel.app/")); // frontend we deployed on vercel
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+        config.setAllowedHeaders(List.of("Content-Type", "Authorization", "X-API-KEY"));
+        config.setAllowCredentials(true); // required for `withCredentials: true`
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
